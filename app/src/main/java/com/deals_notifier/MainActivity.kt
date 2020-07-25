2d4751a0e2fb41a316eaf7622eaba0d4ceb2a39a
Michael Dombrovsky <michaeldombrovsky@gmail.com>
2020-07-25 16:19:12 -0400

    Got rid of unused commented out code.

--- app/src/main/java/com/deals_notifier/MainActivity.kt
+++ app/src/main/java/com/deals_notifier/MainActivity.kt
@@ -10,32 +10,25 @@ import com.google.android.material.tabs.TabLayoutMediator
 class MainActivity : AppCompatActivity() {
 
 
-//    lateinit var dealRecyclerView: /
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        var admin: Admin = Admin(this,findViewById(R.id.dealRecyclerView))
 
-//        var context: Context = this
-//            admin.refresh()
-
-        val viewPager2:ViewPager2 = findViewById(R.id.view_pager)
+        val viewPager2: ViewPager2 = findViewById(R.id.view_pager)
         viewPager2.adapter = TabAdapter(this)
 
         val tabs: TabLayout = findViewById(R.id.tabs)
 
-        val tabLayoutMediator= TabLayoutMediator(tabs, viewPager2,
-            TabLayoutMediator.TabConfigurationStrategy(){ tab: TabLayout.Tab, i: Int ->
-            when(i){
-                0 -> tab.text = "Deals"
-                1 -> tab.text = "Queries"
-            }
-        })
+        val tabLayoutMediator = TabLayoutMediator(tabs, viewPager2,
+            TabLayoutMediator.TabConfigurationStrategy() { tab: TabLayout.Tab, i: Int ->
+                when (i) {
+                    0 -> tab.text = "Deals"
+                    1 -> tab.text = "Queries"
+                }
+            })
 
         tabLayoutMediator.attach()
-
 
 
     }
