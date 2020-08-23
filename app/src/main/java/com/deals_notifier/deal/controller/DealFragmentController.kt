7db76b6e4e5276542f69dda57b8b37502c18b1bb
Mike <michaeldombrovsky@gmail.com>
2020-08-23 16:13:13 -0400

    Made it so that notifications aren't spammed during the first refresh.

--- app/src/main/java/com/deals_notifier/deal/controller/DealFragmentController.kt
+++ app/src/main/java/com/deals_notifier/deal/controller/DealFragmentController.kt
@@ -8,9 +8,7 @@ import com.deals_notifier.deal.ui.DealFragment
 import com.deals_notifier.post.model.Post
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
-import kotlin.concurrent.fixedRateTimer
 
 class DealFragmentController(
     private val validDealHolder: ValidDealHolder,
@@ -28,11 +26,18 @@ class DealFragmentController(
             dealFragment.swipeRefreshLayout.isRefreshing = true
         }
         var newPosts: List<Post> = ArrayList<Post>()
+
+        val firstRefresh: Boolean = dealController.getSize() == 0
+
         CoroutineScope(IO).launch {
             newPosts = dealController.refresh()
         }.invokeOnCompletion {
             dealFragment.swipeRefreshLayout.isRefreshing = false
-            dealNotificationManager.sendDealNotifications(newPosts)
+
+            //Don't spam the notifications if it is the first refresh
+            if (!firstRefresh) {
+                dealNotificationManager.sendDealNotifications(newPosts)
+            }
         }
     }
 
