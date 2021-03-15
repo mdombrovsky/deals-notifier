1e17d6385ce2e4f9bd0805ff9fcbee705ddbdd5c
Noah Hinderle <nhinderl@uoguelph.ca>
2021-03-15 14:52:28 -0400

    Added functionality to collapse and expand each query
    Remotely peer programmed by Noah and Reza

--- app/src/main/java/com/deals_notifier/query/ui/QueryAdapter.kt
+++ app/src/main/java/com/deals_notifier/query/ui/QueryAdapter.kt
@@ -16,6 +16,7 @@ class QueryAdapter(val controller: QueryController) :
     RecyclerView.Adapter<QueryAdapter.ViewHolder>() {
 
     private val viewPool = RecyclerView.RecycledViewPool()
+    private var isCollapsed = true
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater: LayoutInflater = LayoutInflater.from(parent.context)
@@ -27,16 +28,25 @@ class QueryAdapter(val controller: QueryController) :
         return this.controller.getSize()
     }
 
+    private fun setQueryVisibility(holder: ViewHolder) {
+        holder.savedQuery.visibility = if (!isCollapsed) View.VISIBLE else View.GONE
+        holder.closeQueryButton.visibility = if (!isCollapsed) View.VISIBLE else View.GONE
+        holder.dropDownQueryButton.visibility = if (isCollapsed) View.VISIBLE else View.GONE
+    }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val newAdapter = controller.createCriteriaAdapter(position)
         holder.queryTitle.text = controller.getQueryTitle(position)
-        holder.recyclerView.apply {
-            layoutManager =
-                LinearLayoutManager(holder.recyclerView.context, RecyclerView.HORIZONTAL, false)
-            adapter = newAdapter
-        }
 
-        holder.recyclerView.setRecycledViewPool(viewPool)
+        setQueryVisibility(holder)
+
+//        holder.recyclerView.apply {
+//            layoutManager =
+//                LinearLayoutManager(holder.recyclerView.context, RecyclerView.HORIZONTAL, false)
+//            adapter = newAdapter
+//        }
+
+//        holder.recyclerView.setRecycledViewPool(viewPool)
 
 //        holder.addCriteriaButton.setOnClickListener {
 //            newAdapter.controller.add()
@@ -49,6 +59,16 @@ class QueryAdapter(val controller: QueryController) :
                 onSuccess =
                 { text: String -> controller.setQueryTitle(holder.adapterPosition, text) }
             )
+        }
+
+        holder.dropDownQueryButton.setOnClickListener {
+            isCollapsed = false
+            setQueryVisibility(holder)
+        }
+
+        holder.closeQueryButton.setOnClickListener {
+            isCollapsed = true
+            setQueryVisibility(holder)
         }
 
         holder.deleteQueryButton.setOnClickListener {
@@ -65,10 +85,13 @@ class QueryAdapter(val controller: QueryController) :
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val queryTitle: TextView = itemView.queryTitle
-        val recyclerView: RecyclerView = itemView.searchColumnRecyclerView
+//        val recyclerView: RecyclerView = itemView.searchColumnRecyclerView
+        val savedQuery: TextView = itemView.saved_query
         val editQueryTitle: ImageButton = itemView.editQueryTitleButton
 //        val addCriteriaButton: Button = itemView.addColumnButton
         val deleteQueryButton: ImageButton = itemView.deleteQueryButton
+        val closeQueryButton: ImageButton = itemView.close_query
+        val dropDownQueryButton: ImageButton = itemView.dropdown_query
         val onOffSwitchQueryButton: SwitchCompat = itemView.queryOnOffSwitch
     }
 }
